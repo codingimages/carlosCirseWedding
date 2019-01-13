00f1410c34cb610c3b89ad0a68278b0a91bf5bc0
Reynaldo Navedo <rnavedojr@outlook.com>
2019-01-12 23:57:25 -0500

    Initial commit

---
+++ scrollfire.js
@@ -0,0 +1,349 @@
+// Fire scroll section
+    window.sr = ScrollReveal();
+    sr.reveal('.nav', {
+      duration: 2000,
+      origin: 'top',
+      mobile: true,
+      reset: true,
+    });
+    sr.reveal('.bar', {
+      duration: 2000,
+      origin: 'top',
+      mobile: true,
+      reset: true,
+    });
+    sr.reveal('.img-1', { 
+      duration: 1000,
+      origin: 'bottom',
+      mobile: true,
+      reset: false,
+      delay: 25,
+    });
+    sr.reveal('.img-2', { 
+      duration: 1000,
+      origin: 'bottom',
+      mobile: true,
+      reset: false,
+      delay: 50,
+    });
+    sr.reveal('.img-3', { 
+      duration: 1000,
+      origin: 'bottom',
+      mobile: true,
+      reset: false,
+      delay: 75,
+    });
+    sr.reveal('.img-4', { 
+      duration: 1000,
+      origin: 'bottom',
+      mobile: true,
+      reset: false,
+      delay: 100,
+    });
+    sr.reveal('.img-5', { 
+      duration: 1000,
+      origin: 'bottom',
+      mobile: true,
+      reset: false,
+      delay: 125,
+    });
+    sr.reveal('.img-6', { 
+      duration: 1000,
+      origin: 'bottom',
+      mobile: true,
+      reset: false,
+      delay: 150,
+    });
+    sr.reveal('.img-7', { 
+      duration: 1000,
+      origin: 'bottom',
+      mobile: true,
+      reset: false,
+      delay: 175,
+    });
+    sr.reveal('.img-8', { 
+      duration: 1000,
+      origin: 'bottom',
+      mobile: true,
+      reset: false,
+      delay: 200,
+    });
+    sr.reveal('.img-9', { 
+      duration: 1000,
+      origin: 'bottom',
+      mobile: true,
+      reset: false,
+      delay: 225,
+    });
+    sr.reveal('.img-10', { 
+      duration: 1000,
+      origin: 'bottom',
+      mobile: true,
+      reset: false,
+      delay: 250,
+    });    sr.reveal('.img-11', { 
+      duration: 1000,
+      origin: 'bottom',
+      mobile: true,
+      reset: false,
+      delay: 275,
+    });
+    sr.reveal('.img-12', { 
+      duration: 1000,
+      origin: 'bottom',
+      mobile: true,
+      reset: false,
+      delay: 300,
+    });
+    sr.reveal('.img-13', { 
+      duration: 1000,
+      origin: 'bottom',
+      mobile: true,
+      reset: false,
+      delay: 325,
+    });
+    sr.reveal('.img-14', { 
+      duration: 1000,
+      origin: 'bottom',
+      mobile: true,
+      reset: false,
+      delay: 350,
+    });
+    sr.reveal('.img-15', { 
+      duration: 1000,
+      origin: 'bottom',
+      mobile: true,
+      reset: false,
+      delay: 375,
+    });    sr.reveal('.img-16', { 
+      duration: 1000,
+      origin: 'bottom',
+      mobile: true,
+      reset: false,
+      delay: 400,
+    });
+    sr.reveal('.img-17', { 
+      duration: 1000,
+      origin: 'bottom',
+      mobile: true,
+      reset: false,
+      delay: 425,
+    });
+    sr.reveal('.img-18', { 
+      duration: 1000,
+      origin: 'bottom',
+      mobile: true,
+      reset: false,
+      delay: 450,
+    });
+    sr.reveal('.img-19', { 
+      duration: 1000,
+      origin: 'bottom',
+      mobile: true,
+      reset: false,
+      delay: 475,
+    });
+    sr.reveal('.img-20', { 
+      duration: 1000,
+      origin: 'bottom',
+      mobile: true,
+      reset: false,
+      delay: 500,
+    });    sr.reveal('.img-21', { 
+      duration: 1000,
+      origin: 'bottom',
+      mobile: true,
+      reset: false,
+      delay: 525,
+    });
+    sr.reveal('.img-22', { 
+      duration: 1000,
+      origin: 'bottom',
+      mobile: true,
+      reset: false,
+      delay: 550,
+    });
+    sr.reveal('.img-23', { 
+      duration: 1000,
+      origin: 'bottom',
+      mobile: true,
+      reset: false,
+      delay: 575,
+    });
+    sr.reveal('.img-24', { 
+      duration: 1000,
+      origin: 'bottom',
+      mobile: true,
+      reset: false,
+      delay: 600,
+    });
+    sr.reveal('.img-25', { 
+      duration: 1000,
+      origin: 'bottom',
+      mobile: true,
+      reset: false,
+      delay: 625,
+    });    sr.reveal('.img-26', { 
+      duration: 1000,
+      origin: 'bottom',
+      mobile: true,
+      reset: false,
+      delay: 650,
+    });
+    sr.reveal('.img-27', { 
+      duration: 1000,
+      origin: 'bottom',
+      mobile: true,
+      reset: false,
+      delay: 675,
+    });
+    sr.reveal('.img-28', { 
+      duration: 1000,
+      origin: 'bottom',
+      mobile: true,
+      reset: false,
+      delay: 700,
+    });
+    sr.reveal('.img-29', { 
+      duration: 1000,
+      origin: 'bottom',
+      mobile: true,
+      reset: false,
+      delay: 725,
+    });
+    sr.reveal('.img-30', { 
+      duration: 1000,
+      origin: 'bottom',
+      mobile: true,
+      reset: false,
+      delay: 750,
+    });    sr.reveal('.img-31', { 
+      duration: 1000,
+      origin: 'bottom',
+      mobile: true,
+      reset: false,
+      delay: 775,
+    });
+    sr.reveal('.img-32', { 
+      duration: 1000,
+      origin: 'bottom',
+      mobile: true,
+      reset: false,
+      delay: 800,
+    });
+    sr.reveal('.img-33', { 
+      duration: 1000,
+      origin: 'bottom',
+      mobile: true,
+      reset: false,
+      delay: 825,
+    });
+    sr.reveal('.img-34', { 
+      duration: 1000,
+      origin: 'bottom',
+      mobile: true,
+      reset: false,
+      delay: 850,
+    });
+    sr.reveal('.img-35', { 
+      duration: 1000,
+      origin: 'bottom',
+      mobile: true,
+      reset: false,
+      delay: 875,
+    });    sr.reveal('.img-36', { 
+      duration: 1000,
+      origin: 'bottom',
+      mobile: true,
+      reset: false,
+      delay: 900,
+    });
+    sr.reveal('.img-37', { 
+      duration: 1000,
+      origin: 'bottom',
+      mobile: true,
+      reset: false,
+      delay: 925,
+    });
+    sr.reveal('.img-38', { 
+      duration: 1000,
+      origin: 'bottom',
+      mobile: true,
+      reset: false,
+      delay: 950,
+    });
+    sr.reveal('.img-39', { 
+      duration: 1000,
+      origin: 'bottom',
+      mobile: true,
+      reset: false,
+      delay: 1000,
+    });
+    sr.reveal('.img-40', { 
+      duration: 1000,
+      origin: 'bottom',
+      mobile: true,
+      reset: false,
+      delay: 1025,
+    });    sr.reveal('.img-41', { 
+      duration: 1000,
+      origin: 'bottom',
+      mobile: true,
+      reset: false,
+      delay: 1050,
+    });
+    sr.reveal('.img-42', { 
+      duration: 1000,
+      origin: 'bottom',
+      mobile: true,
+      reset: false,
+      delay: 1075,
+    });
+    sr.reveal('.img-43', { 
+      duration: 1000,
+      origin: 'bottom',
+      mobile: true,
+      reset: false,
+      delay: 1100,
+    });
+    sr.reveal('.img-44', { 
+      duration: 1000,
+      origin: 'bottom',
+      mobile: true,
+      reset: false,
+      delay: 1125,
+    });
+    sr.reveal('.img-45', { 
+      duration: 1000,
+      origin: 'bottom',
+      mobile: true,
+      reset: false,
+      delay: 1150,
+    });    sr.reveal('.img-46', { 
+      duration: 1000,
+      origin: 'bottom',
+      mobile: true,
+      reset: false,
+      delay: 1175,
+    });
+    sr.reveal('.img-47', { 
+      duration: 1000,
+      origin: 'bottom',
+      mobile: true,
+      reset: false,
+      delay: 1200,
+    });
+    sr.reveal('.img-48', { 
+      duration: 1000,
+      origin: 'bottom',
+      mobile: true,
+      reset: false,
+      delay: 1225,
+    });
+    sr.reveal('.img-49', { 
+      duration: 1000,
+      origin: 'bottom',
+      mobile: true,
+      reset: false,
+      delay: 1250,
+    });
